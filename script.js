541ced601be4d2aeaea5cea6c7edb00469325d7c
Alexander Mac <amac2@student.mtsac.edu>
2019-05-24 10:08:06 -0700

    all 18 cards ready with pictures

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var secondCardClicked = null;
 var matches = null;
 var imgCard1 = null;
 var imgCard2 = null;
+var max_matches = 9;
 
 
 $(document).ready(initialize);
@@ -11,6 +12,32 @@ $(document).ready(initialize);
 function initialize() {
     console.log('Initialized!');
     $('.card').click(handleCardClick);
+
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    };
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    };
+
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
 
 }
 
@@ -46,8 +73,11 @@ function handleCardClick(event) {
         else if (imgCard1 !== imgCard2) {
             setTimeout(flipCardsBack, 1500);
         }
-
     }
+     if (matches === max_matches) {
+         console.log('You won!!!');
+         $('#myModal').css('display', 'block');
+     }
 
 }
 
@@ -57,4 +87,5 @@ function flipCardsBack() {
     firstCardClicked = null;
     secondCardClicked = null;
 }
+
 
