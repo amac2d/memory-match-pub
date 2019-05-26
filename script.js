39e1ddaba4e990742f7191e0083d6292d8f53217
Alexander Mac <amac2@student.mtsac.edu>
2019-05-25 19:14:41 -0700

    Clean up on index and css files, working on multiclicks

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var firstCardClicked = null;
 var secondCardClicked = null;
 var matches = null;
 var max_matches = 9;
-var attempts = null;
+var attempts = 0;
 var games_played = null;
 
 
@@ -12,43 +12,25 @@ $(document).ready(initialize);
 function initialize() {
     console.log('Initialized!');
     $('.card').bind('click', handleCardClick);
+    $('#myBtn').bind('click', showModal);
+    $('.close').bind('click', closeModal);
+    $('#myModal').bind('click', closeModal);
+    $('#resetButton').bind('click', resetStats);
 
-    // Get the modal
-    var modal = document.getElementById("myModal");
+}
 
-// Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-    btn.onclick = function() {
-        modal.style.display = "block";
-    };
-
-// When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    };
-
-// When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    };
-
-
-    $('#resetButton').click(resetStats);
-
+function showModal() {
+    $('#myModal').show();
+    $('.modal-content').show();
+}
+function closeModal() {
+    $('#myModal').hide();
+    $('.modal-content').hide();
 }
 
 
 function handleCardClick(event) {
     $(event.currentTarget).find('.back').addClass('hidden');
-
-
 
     if (firstCardClicked === null) {
         firstCardClicked = $(event.currentTarget);
@@ -70,7 +52,8 @@ function handleCardClick(event) {
         displayStats();
     }
      if (matches === max_matches) {
-         $('#myModal').css('display', 'block');
+         // $('#myModal').css('display', 'block');
+         showModal();
      }
 
 }
@@ -108,7 +91,6 @@ function resetStats() {
     games_played++;
     displayStats();
     $('.back').removeClass('hidden');
-
-    $('#myModal').hide();
+    closeModal();
 
 }
